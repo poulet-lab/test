29f8564e766a972702021fc44967885957b5856a
bimac <bimac@users.noreply.github.com>
2020-05-26 23:32:10 +0200

    Update intrinsic.m

--- @intrinsic/intrinsic.m
+++ @intrinsic/intrinsic.m
@@ -1,7 +1,7 @@
 classdef intrinsic < handle & matlab.mixin.CustomDisplay
 
     properties %(Access = private)
-        Version         = '1.0.0-beta1';
+        Version         = '1.0.0-beta2';
         Flags
 
         h               = [] 	% handles
